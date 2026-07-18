7dc1cd34e9e190132499e6a09fc456ce32313dfc
Matheus Ferraz <148595215+math3usferraz@users.noreply.github.com>
2026-07-18 18:49:11 +0000

    criei um botão e uma frase meio triste seila são testes

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,8 @@
 function home() {
-   return <h1> Oi amor, preciso de ajuda </h1>
+   return <h1> Oi amor, preciso de ajuda, a vida tem ficado complicada, as pessoas tem me feito mal, só queria que tudo melhorasse, vou criar aqui dois botões, devo continuar ou não devo continuar, eai qual a repsosta?
+      <button>devo continuar</button>
+      <button>não devo continuar</button>
+   </h1>
 }
 
 export default home;
