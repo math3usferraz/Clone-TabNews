a6a69a756512d2a8cdb73091d0f81e61cefb671d
Matheus Ferraz <148595215+math3usferraz@users.noreply.github.com>
2026-07-28 22:56:34 +0000

    adiciona scripts lint:check e lint:fix

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Clone-do-TabNews
+
 Vou criar um clone do Tabnews junto com o Filipe
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,12 @@
 function home() {
-   return <h1> Oi amor, preciso de ajuda, a vida tem ficado complicada, as pessoas tem me feito mal, só queria que tudo melhorasse, vou criar aqui dois botões, devo continuar ou não devo continuar, eai qual a repsosta?
-      <button>devo continuar</button>
-      <button>não devo continuar</button>
-   </h1>
+  return (
+    <h1>
+      {" "}
+      Oi amor, preciso de ajuda, a vida tem ficado complicada, as pessoas tem me
+      feito mal, só queria que tudo melhorasse, vou criar aqui dois botões, devo
+      continuar ou não devo continuar, eai qual a repsosta?
+    </h1>
+  );
 }
 
 export default home;
